6c5030df9f14fbae5d7377229fad41ddd2a69ccc
Jakub Królik <01142179@pw.edu.pl>
2021-04-15 00:05:24 +0200

    row-59 [only-ui]

---
+++ src/bikesList.tsx
@@ -0,0 +1,11 @@
+import './App.css';
+import './Layout/topbar.tsx';
+
+function BikeListPage() {
+    return (
+      <div>  
+        
+      </div>
+    );
+  }  
+export default BikeListPage;
---
+++ src/stationList.tsx
@@ -0,0 +1,14 @@
+import './App.css';
+import './Layout/topbar.tsx';
+
+
+function StationListPage() {
+    return (
+      <div className="App" style={{ height: "91vh", display: "flex", flexDirection: "column" }}>  
+       
+      </div>
+    );
+  }
+
+  
+export default StationListPage;
