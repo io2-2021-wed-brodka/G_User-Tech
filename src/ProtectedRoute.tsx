3dec2ab2819da9eab93ed425470055ddd4f86975
Jakub Królik <01142179@pw.edu.pl>
2021-04-24 19:30:53 +0200

    xD zapomniałęm zrobić commita

---
+++ src/ProtectedRoute.tsx
@@ -0,0 +1,20 @@
+import { ReactNode } from "react"
+import { Redirect, Route } from "react-router-dom"
+import { LoggedIn } from "./Layout/topbar"
+
+interface IProps {
+    children: ReactNode;
+    path?: string;
+}
+
+export const ProtectedRoute = ({ children, ...props } : IProps) => {
+    return (
+        <Route {...props}>
+            {LoggedIn() ?
+                children
+                : 
+                <Redirect to='/login'/>
+            }
+        </Route>
+      )
+  }
