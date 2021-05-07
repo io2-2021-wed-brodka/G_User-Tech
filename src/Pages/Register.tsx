21213de517574996be67b3cdc5ad89360f49f890
Arkadiusz Górecki <53404789+arkadiusz-gorecki@users.noreply.github.com>
2021-05-07 16:41:38 +0200

    [BUG] Enter key not working properly for register (#7)
    * Fixed enter key for register and texts on register page
    
    * Changed enter handling
    
    * Changed login and register page

--- src/Pages/Register.tsx
+++ src/Pages/Register.tsx
@@ -39,8 +39,13 @@ export const RegisterLoginPage = () =>{
     }
     const onEnterDown = (event : any) => {
         if(event.key == "Enter") {
-          event.preventDefault();
-          handleLogging();
+            event.preventDefault();
+            if (loginOpen) 
+                handleLogging() 
+            else {
+                handleRegister();
+                handleOpen(); // after register (succesful or not) switch to login page
+            }
         }
     }
     return (
@@ -50,7 +55,7 @@ export const RegisterLoginPage = () =>{
                 <Box p={1} m={1}>
                     {loginOpen ?
                         <Animated animationIn="zoomIn" animationOut="zoomOut" isVisible={loginOpen}   >
-                            <Container fixed className={classes.formContainer} >
+                            <Container fixed className={classes.formContainerBlue} >
                                 <div className={classes.welcomeLabel}>Log in</div>
                                 <TextField id="standard-login" label="Login" variant="filled" className={classes.textFieldStyle}
                                     onChange={(event: any) => handleChangeLoginLogin(event.target.value)} onKeyDown={onEnterDown}/>
@@ -60,33 +65,33 @@ export const RegisterLoginPage = () =>{
                             </Container>
                         </Animated>              
                     :  
-                        <Container fixed className={classes.formContainerOverlay}>
+                        <Container fixed className={classes.formContainerBlue}>
                             <div className={classes.welcomeLabel}>Hello, Friend!</div>
                             <div className={classes.welcomeLabelSmall}>Enter your personal data and begin journey with us</div>
-                            <div className={classes.welcomeLabelSmall}>Don't have account yet? Sing up here!</div>
-                            <Button variant="outlined" onClick={() => handleOpen()} style={{borderRadius: '15px'}}>Sign up</Button>
+                            <div className={classes.welcomeLabelSmall}>Have account already?</div>
+                            <Button variant="outlined" onClick={() => handleOpen()} style={{borderRadius: '15px'}}>Log in</Button>
                         </Container>                      
                     }
                 </Box>
                 <Box p={1} m={1}>
                     {signInOpen ?
                         <Animated animationIn="zoomIn" animationOut="zoomOut" isVisible={signInOpen}>
-                            <Container fixed className={classes.formContainer}>
-                                <div className={classes.welcomeLabel}>Sign in</div>
+                            <Container fixed className={classes.formContainerRed}>
+                                <div className={classes.welcomeLabel}>Sign up</div>
                                 <TextField id="standard-login" label="Login" variant="filled" className={classes.textFieldStyle}
                                     onChange={(event: any) => handleChangeLoginRegister(event.target.value)} onKeyDown={onEnterDown}/>
                                 <TextField id="standard-password" type="password" label="Password" variant="filled" className={classes.textFieldStyle}
                                     onChange={(event: any) => handleChangePasswordRegister(event.target.value)} onKeyDown={onEnterDown}/>
                                 {/* <TextField id="standard-password-confirm" label="Password Confirm" variant="filled" className={classes.textFieldStyle}/> */}
-                                <Button variant="contained" style={{borderRadius: '15px'}} onClick={() => {handleRegister(); handleOpen()} }> Sign in</Button>
+                                <Button variant="contained" style={{borderRadius: '15px'}} onClick={() => {handleRegister(); handleOpen()} }> Sign up</Button>
                             </Container>
                         </Animated>
                     :
-                        <Container fixed className={classes.formContainerOverlay}>
+                        <Container fixed className={classes.formContainerRed}>
                             <div className={classes.welcomeLabel}>Welcome Back!</div>
                             <div className={classes.welcomeLabelSmall}>To keep conected with us please login with your personal data</div>
-                            <div className={classes.welcomeLabelSmall}>Have account already? Sign in here!</div>
-                            <Button variant="outlined" onClick={() => handleOpen()} style={{borderRadius: '15px'}}>Sign in</Button>
+                            <div className={classes.welcomeLabelSmall}>Don't have account yet?</div>
+                            <Button variant="outlined" onClick={() => handleOpen()} style={{borderRadius: '15px'}}>Sign up</Button>
                         </Container>           
             }
                 </Box>
