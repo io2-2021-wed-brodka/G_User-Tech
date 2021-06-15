39bd82972d7f478929c107bf243d86fce1e54613
Tomek Karwowski <to.karwowski@gmail.com>
2021-06-15 09:17:42 +0200

    use deployed backend

--- src/Api/urls.ts
+++ src/Api/urls.ts
@@ -1,1 +1,4 @@
-export const BASE_URL = "http://127.0.0.1:8080/"
+const env = process.env.REACT_APP_STAGE;
+
+const prodUrl = 'http://ec2-18-156-197-166.eu-central-1.compute.amazonaws.com:8080/';
+export const BASE_URL = env === 'prod' ? prodUrl : 'http://localhost:8000';
