a549b39fcf04ae339999a425cf413e4ae6eef11d
Jakub Królik <01142179@pw.edu.pl>
2021-06-15 16:12:52 +0200

    Adding footer

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
 import "./Layout/topbar.tsx";
 import { TopBar } from "./Layout/topbar";
+import { Footer } from "./Layout/footer";
 import StationBikeListPage from "./Pages/stationBikesListPage";
 import StationListPage from "./Pages/stationList";
 import RentedBikesListPage from "./Pages/rentedBikesList";
@@ -16,39 +17,42 @@ import { BikesListPage } from "./Pages/bikesListPage";
 export default function App() {
   const classes = useStyles();
   return (
-    <div className={classes.webpageStyle}>
-      <Router>
-        <div>
-          <TopBar />
-          <Switch>
-            <ProtectedRoute path="/stations/:id/bikes">
-              <StationBikeListPage />
-            </ProtectedRoute>
-            <ProtectedRoute path="/stations/active">
-              <StationListPage />
-            </ProtectedRoute>
-            <ProtectedRoute path="/bikes/rented">
-              <RentedBikesListPage />
-            </ProtectedRoute>
-            <ProtectedRoute path="/bikes/reserved">
-              <ReservedBikesListPage />
-            </ProtectedRoute>
-            <Route path="/login">
-              <RegisterLoginPage />
-            </Route>
-            <ProtectedRoute path="/main-menu">
-              <MainMenuPage />
-            </ProtectedRoute>
-            <ProtectedRoute path="/malfunctions">
-              <MalfunctionsListPage/>
-            </ProtectedRoute>
-            <ProtectedRoute path="/bikes">
-              <BikesListPage />
-            </ProtectedRoute>
-            <Route path="/"></Route>
-          </Switch>
-        </div>
-      </Router>
+    <div>
+      <div className={classes.webpageStyle}>
+        <Router>
+          <div>
+            <TopBar />
+            <Switch>
+              <ProtectedRoute path="/stations/:id/bikes">
+                <StationBikeListPage />
+              </ProtectedRoute>
+              <ProtectedRoute path="/stations/active">
+                <StationListPage />
+              </ProtectedRoute>
+              <ProtectedRoute path="/bikes/rented">
+                <RentedBikesListPage />
+              </ProtectedRoute>
+              <ProtectedRoute path="/bikes/reserved">
+                <ReservedBikesListPage />
+              </ProtectedRoute>
+              <Route path="/login">
+                <RegisterLoginPage />
+              </Route>
+              <ProtectedRoute path="/main-menu">
+                <MainMenuPage />
+              </ProtectedRoute>
+              <ProtectedRoute path="/malfunctions">
+                <MalfunctionsListPage />
+              </ProtectedRoute>
+              <ProtectedRoute path="/bikes">
+                <BikesListPage />
+              </ProtectedRoute>
+              <Route path="/"></Route>
+            </Switch>
+          </div>
+        </Router>
+      </div>
+      <Footer />
     </div>
   );
 }
--- src/Styles/style.ts
+++ src/Styles/style.ts
@@ -1,163 +1,188 @@
-import {createStyles, makeStyles, Theme, createMuiTheme} from '@material-ui/core/styles';
-import bicycleWallpaper from '../Resources/bikeWP.jpg';
+import {
+  createStyles,
+  makeStyles,
+  Theme,
+  createMuiTheme,
+} from "@material-ui/core/styles";
+import bicycleWallpaper from "../Resources/bikeWP.jpg";
 export const useStyles = makeStyles((theme: Theme) =>
-    createStyles({
-        pageTitle: {
-            alignSelf: "center", 
-            color: "white", 
-            textShadow: "0px 0px 5px black",
-        },
-        ListStyle: {
-            overflowY: 'auto',
-            opacity: '0.92',
-            marginLeft: '10%',
-            marginRight: '10%',
-            marginTop: '2%',
-        },
-        ListFont: {
-            color: 'white'
-        },
-        listSection: {
-            backgroundColor: 'inherit',
-        },
-        ul: {
-            backgroundColor: 'inherit',
-            padding: 0,
-        },
-        formControl: {
-            margin: theme.spacing(1),
-            minWidth: 120,
-        },
-        container: {
-            display: 'flex',
-            flexWrap: 'wrap',
-        },
-        rentButton: {
-            backgroundColor: '#f2e20e',
-            variant: 'contained',
-            margin: '5px'
-        },
-        blockButton: {
-            backgroundColor: '#f2e20e',
-            variant: 'contained',
-            margin: '5px'
-        },
-        cancelReservationButton: {
-            backgroundColor: '#D11A2A ',
-            variant: 'contained',
-            margin: '5px'
-        },
-        generalContainer: {
-            height: '91vh',
-            display: 'flex',
-            flexDirection: 'column'
-        },
-        formContainerBlue: {
-            backgroundColor: '#aec6cf',
-            opacity: '0.95',
-            borderRadius: '15px',
-            display: 'flex',
-            flexDirection: 'column',
-            height: '400px',
-            marginTop: '50px',
-            width: '300px',
-            background: 'linear-gradient(rgba(0,0,255,0.5),transparent)',         
-          },
-          textFieldStyle: {
-              margin: '10px',
-              backgroundColor: 'white',
-              color: 'white',         
-          },
-          welcomeLabel: {
-            color: 'white',
-            marginTop: '10%',
-            fontSize: '35px',
-            marginBottom: '20px',
-            textAlign: 'center'
-          },
-          windowContainer: {
-            display: 'flex',
-            flexDirection: 'column',
-          },
-          formContainerRed: {
-            backgroundColor: '#aec6cf',
-            opacity: '0.95',
-            borderRadius: '15px',
-            display: 'flex',
-            flexDirection: 'column',
-            height: '400px',
-            marginTop: '50px',
-            width: '300px',  
-            background: 'linear-gradient(rgba(250,0,0,0.5),transparent)',             
-          },
-          welcomeLabelSmall: {
-            color: 'white',
-            marginTop: '10%',
-            fontSize: '15px',
-            marginBottom: '20px',
-            textAlign: 'center'
-          },
-        returnBikeButton: {
-            backgroundColor: '#f2e20e ',
-            variant: 'contained',
-            margin: '5px'
-        },
-        reserveBikeButton: {
-            backgroundColor: '#ffb347 ',
-            variant: 'contained',
-            margin: '5px'
-        },
-        reportMalfunctionButton: {
-            backgroundColor: '#ff581a ',
-            variant: 'contained',
-            margin: '5px',
-            lineHeight: 0.95
-        },
-        malfunctionDescriptionButton: {
-            backgroundColor: '#2ca5b8 ',
-            variant: 'contained',
-            margin: '5px',
-        },
-        listSubheaderStyle: {
-          backgroundColor: "#4E4E50",
-          color: "white",
-          display: "flex",
-          height: "50px",
-          marginBottom: "5px",
-          marginTop: "5px",
-          borderRadius: "15px",
-        },
-        listBox: {
-          display: "flex",
-          flexDirection: "row",
-          padding: theme.spacing(0),
-          margin: theme.spacing(1),
-          alignSelf: "center",
-        },
-        listItemStyle: {
-            backgroundColor: '#69696e',
-            color: 'white',
-            display: 'flex',
-            height: '50px',
-            marginBottom: '5px',
-            marginTop: '5px',
-            borderRadius: '15px',
-        },
-        webpageStyle: {
-            height: "100vh",
-            display: "flex",
-            flexDirection: "column",
-            width: '100%',
-            backgroundImage: `url(${bicycleWallpaper})`,
-            backgroundRepeat: 'no-repeat',
-            backgroundSize: 'cover',
-        }
-    }),
+  createStyles({
+    pageTitle: {
+      alignSelf: "center",
+      color: "white",
+      textShadow: "0px 0px 5px black",
+    },
+    ListStyle: {
+      overflowY: "auto",
+      opacity: "0.92",
+      marginLeft: "10%",
+      marginRight: "10%",
+      marginTop: "2%",
+    },
+    ListFont: {
+      color: "white",
+    },
+    listSection: {
+      backgroundColor: "inherit",
+    },
+    ul: {
+      backgroundColor: "inherit",
+      padding: 0,
+    },
+    formControl: {
+      margin: theme.spacing(1),
+      minWidth: 120,
+    },
+    container: {
+      display: "flex",
+      flexWrap: "wrap",
+    },
+    rentButton: {
+      backgroundColor: "#f2e20e",
+      variant: "contained",
+      margin: "5px",
+    },
+    blockButton: {
+      backgroundColor: "#f2e20e",
+      variant: "contained",
+      margin: "5px",
+    },
+    cancelReservationButton: {
+      backgroundColor: "#D11A2A ",
+      variant: "contained",
+      margin: "5px",
+    },
+    generalContainer: {
+      height: "91vh",
+      display: "flex",
+      flexDirection: "column",
+    },
+    formContainerBlue: {
+      backgroundColor: "#aec6cf",
+      opacity: "0.95",
+      borderRadius: "15px",
+      display: "flex",
+      flexDirection: "column",
+      height: "400px",
+      marginTop: "50px",
+      width: "300px",
+      background: "linear-gradient(rgba(0,0,255,0.5),transparent)",
+    },
+    textFieldStyle: {
+      margin: "10px",
+      backgroundColor: "white",
+      color: "white",
+    },
+    welcomeLabel: {
+      color: "white",
+      marginTop: "10%",
+      fontSize: "35px",
+      marginBottom: "20px",
+      textAlign: "center",
+    },
+    windowContainer: {
+      display: "flex",
+      flexDirection: "column",
+    },
+    formContainerRed: {
+      backgroundColor: "#aec6cf",
+      opacity: "0.95",
+      borderRadius: "15px",
+      display: "flex",
+      flexDirection: "column",
+      height: "400px",
+      marginTop: "50px",
+      width: "300px",
+      background: "linear-gradient(rgba(250,0,0,0.5),transparent)",
+    },
+    welcomeLabelSmall: {
+      color: "white",
+      marginTop: "10%",
+      fontSize: "15px",
+      marginBottom: "20px",
+      textAlign: "center",
+    },
+    returnBikeButton: {
+      backgroundColor: "#f2e20e ",
+      variant: "contained",
+      margin: "5px",
+    },
+    reserveBikeButton: {
+      backgroundColor: "#ffb347 ",
+      variant: "contained",
+      margin: "5px",
+    },
+    reportMalfunctionButton: {
+      backgroundColor: "#ff581a ",
+      variant: "contained",
+      margin: "5px",
+      lineHeight: 0.95,
+    },
+    malfunctionDescriptionButton: {
+      backgroundColor: "#2ca5b8 ",
+      variant: "contained",
+      margin: "5px",
+    },
+    listSubheaderStyle: {
+      backgroundColor: "#4E4E50",
+      color: "white",
+      display: "flex",
+      height: "50px",
+      marginBottom: "5px",
+      marginTop: "5px",
+      borderRadius: "15px",
+    },
+    listBox: {
+      display: "flex",
+      flexDirection: "row",
+      padding: theme.spacing(0),
+      margin: theme.spacing(1),
+      alignSelf: "center",
+    },
+    listItemStyle: {
+      backgroundColor: "#69696e",
+      color: "white",
+      display: "flex",
+      height: "50px",
+      marginBottom: "5px",
+      marginTop: "5px",
+      borderRadius: "15px",
+    },
+    webpageStyle: {
+      height: "100vh",
+      display: "flex",
+      flexDirection: "column",
+      width: "100%",
+      backgroundImage: `url(${bicycleWallpaper})`,
+      backgroundRepeat: "no-repeat",
+      backgroundSize: "cover",
+    },
+    footerStyle: {
+      backgroundColor: "#000000",
+      fontSize: "20px",
+      color: "white",
+      borderTop: "1px solid #000000",
+      textAlign: "center",
+
+      left: "0",
+      bottom: "0",
+      height: "100%",
+      width: "100%",
+      opacity: "80%",
+    },
+    footerContainer: {
+      display: "flex",
+    },
+    footerOneInfo: {
+      marginLeft: "50px",
+      fontSize: "15px",
+    },
+  })
 );
 export const themeWarning = createMuiTheme({
-    palette: {
-        primary: {
-            main: '#950740'
-        }
+  palette: {
+    primary: {
+      main: "#950740",
     },
+  },
 });
